1328149c8c635dc9209bbfc6a26648f5ad1f410e
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-14 22:37:37 +0100

    style header, stars container and key pad divs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ function App() {
     <div className="App">
       <h3>Pick 1 or more number numbers that sum up to the number of stars</h3>
       <div className="stars-container"></div>
-      <div className="key-pad"></div>
+      <div className="key-pad">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
     </div>
   );
 }
