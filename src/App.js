03bc0172f4e9a774617d6df1cb6a4e0f829e65c2
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-14 23:46:18 +0100

    style stars-container and starts component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,56 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const colors = {
+    available: "red",
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+  };
+
+  const utils = {
+    // sum an array
+    sum: arr => arr.reduce((acc, curr) => acc += curr, 0),
+
+    // create an array of numbers between min and max (edges included)
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+    // pick a random number between min and max (edges included)
+    random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+    // Given an array of numbers and a max...
+    // Pick a random sum (< max) from the set of all available sums in arr
+    randomSumIn: (arr, max) => {
+      const sets = [[]];
+      const sums = [];
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0, len = sets.length; j < len; j++) {
+          const candidateSet = sets[j].concat(arr[i]);
+          const candidateSum = utils.sum(candidateSet);
+          if (candidateSum <= max) {
+            sets.push(candidateSet);
+            sums.push(candidateSum);
+          }
+        }
+      }
+      return sums[utils.random(0, sums.length - 1)];
+    }
+  }
+  
   return (
     <div className="App">
       <h3>Pick 1 or more number numbers that sum up to the number of stars</h3>
-      <div className="stars-container"></div>
+      <div className="stars-container">
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+        <div className="star" />
+      </div>
       <div className="key-pad">
         <button>1</button>
         <button>2</button>
