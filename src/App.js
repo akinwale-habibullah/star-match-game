cb993b6a145c3574b3bdfc7467bce41e3f6e0a6a
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-14 22:17:00 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Pick 1 or more number numbers that sum up to the number of stars</h3>
+      <div className="stars-container"></div>
+      <div className="key-pad"></div>
     </div>
   );
 }
