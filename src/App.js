81765056dad6bce4f3b7822c6c60a781c851905d
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-15 00:01:35 +0100

    create keypad button dynamically

--- src/App.js
+++ src/App.js
@@ -48,18 +48,13 @@ function App() {
              <div key={starId} className="star" />
           )
         }
-        
       </div>
       <div className="key-pad">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
+        {
+          utils.range(1, 9).map(buttonId => 
+            <button key={buttonId} className="star">{buttonId}</button>
+          )
+        }
       </div>
     </div>
   );
