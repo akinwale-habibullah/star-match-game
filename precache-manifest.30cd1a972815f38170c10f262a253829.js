33fe571802734a5e6f34b9112a6a39b8bcee727a
Akinwale Habib <akinwalehabib@hotmail.com>
2019-11-28 19:52:51 +0100

    Updates

---
+++ precache-manifest.30cd1a972815f38170c10f262a253829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9cbab4c52818a6aaa2d911474ab37f",
+    "url": "/start-match-game/index.html"
+  },
+  {
+    "revision": "f64d2f6f47f7b3cf1eb0",
+    "url": "/start-match-game/static/css/main.3c85dd11.chunk.css"
+  },
+  {
+    "revision": "cd90d16cac5c751e0c9b",
+    "url": "/start-match-game/static/js/2.0d6b0ad3.chunk.js"
+  },
+  {
+    "revision": "f64d2f6f47f7b3cf1eb0",
+    "url": "/start-match-game/static/js/main.93a15375.chunk.js"
+  },
+  {
+    "revision": "651e2fd79480c62b443e",
+    "url": "/start-match-game/static/js/runtime-main.8adf2742.js"
+  }
+]);
